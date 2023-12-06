d31532e6b337b4719c72b2024a21007dc2c9b26d
Ashwani <vcsashwani@gmail.com>
2023-12-06 13:54:51 +0530

    16-markdown-preview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 function App() {
+  const [markdown, setMarkdown] = useState('# markdown preview')
+
   return (
-    <div className="App">
-      <h1>
-        Reactjs projects
-      </h1>
-    </div>
-  );
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
