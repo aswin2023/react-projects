e76f1e29e5f21ef8f86f1f369a6fde5b2e39dff6
Ashwani <vcsashwani@gmail.com>
2023-12-06 13:18:38 +0530

    14-cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React from 'react'
+import { useGlobalContext } from './context'
 
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+// items
 
 function App() {
+  const { loading } = useGlobalContext()
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <h1>
-        Reactjs projects
-      </h1>
-    </div>
-  );
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
 }
 
-export default App;
+export default App
