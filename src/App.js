d04a084baba576e93a07d9a0b98a8c3b9480b98b
Ashwani <vcsashwani@gmail.com>
2023-12-06 18:22:55 +0530

    21-movie-db

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
+import Home from './Home';
+import Movie from './SingleMovie';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Reactjs projects
-      </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='movies/:id' element={<Movie />} />
+    </Routes>
   );
 }
 
