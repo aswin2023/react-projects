86988a9d69540b1bb13753625a7455fad17a63f9
Ashwani <vcsashwani@gmail.com>
2023-12-06 17:58:38 +0530

    20-dark-mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
 
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
 
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
   return (
-    <div className="App">
-      <h1>
-        Reactjs projects
-      </h1>
-    </div>
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
   );
 }
 
